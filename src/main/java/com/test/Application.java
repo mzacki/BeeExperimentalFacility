272ccb2bc1cec101b45dc6e37ca50276cab80efc
mathud1 <mateusz.hudzikowski@gmail.com>
2019-04-23 20:29:05 +0200

    m

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -4,6 +4,14 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * Created by Matt on 23.04.2019 at 18:32.
@@ -16,12 +24,62 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+
     @Bean
     ApplicationRunner applicationRunner(InsertRepository insertRepository) {
         return args -> {
             insertRepository.save(new Insert("storm"));
             insertRepository.save(new Insert("rain"));
         };
+    }
+
+}
+
+@RestController
+class TestController {
+
+    private final InsertRepository insertRepository;
+
+    @GetMapping("/")
+    String test() {
+        return "Test no. 2 ***REMOVED***ed!";
+    }
+
+    @GetMapping("/inserts")
+    Iterable<Insert> inserts() {
+        return insertRepository.findAll();
+    }
+
+    public TestController(InsertRepository insertRepository) {
+        this.insertRepository = insertRepository;
+    }
+}
+
+interface InsertRepository extends CrudRepository<Insert, Long> {
+}
+
+@Entity
+class Insert {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String message;
+
+    public Insert() {}
+
+    public Insert(String message) {
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
 
--- src/main/java/com/test/InsertRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by Matt on 23.04.2019 at 19:34.
- */
-public interface InsertRepository extends CrudRepository<Insert, Long> {
-}
