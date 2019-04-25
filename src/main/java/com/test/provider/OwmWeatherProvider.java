b60383bc1e72ae89bdce0011bae8a783bab7bc4e
mathud1 <mateusz.hudzikowski@gmail.com>
2019-04-25 18:22:06 +0200

    add thread

---
+++ src/main/java/com/test/provider/OwmWeatherProvider.java
@@ -0,0 +1,52 @@
+package com.test.provider;
+
+import com.test.entity.WeatherData;
+import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.model.CurrentWeather;
+import net.aksingh.owmjapis.model.param.Weather;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by Matt on 11.09.2018 at 20:41.
+ */
+@Component
+public class OwmWeatherProvider {
+
+    private OWM owm;
+
+    public OwmWeatherProvider() {
+        this.owm = new OWM("dcf890fc0ba06ef58dd0ec3a8842e983");
+    }
+
+    public WeatherData getWeatherData() {
+
+
+        CurrentWeather cwd = null;
+
+
+        try {
+            cwd = owm.currentWeatherByCoords(50.081926, 18.675203);
+            //cwd = owm.currentWeatherByCityName(location.getName());
+        } catch (APIException e) {
+            e.printStackTrace();
+        }
+
+
+        String localisation = cwd.getCityName();
+        double windSpeed = cwd.getWindData().getSpeed();
+        double temperature = Math.round(cwd.getMainData().getTemp() - 273.15);
+        double pressure = cwd.getMainData().getPressure();
+        double humidity = cwd.getMainData().getHumidity();
+        double cloudCover = cwd.getCloudData().getCloud();
+        List<Weather> overall = cwd.getWeatherList();
+        String description = String.valueOf(cwd.getWeatherList().get(0).getMoreInfo());
+
+
+        return new WeatherData(temperature, windSpeed, pressure, humidity, cloudCover);
+
+    }
+
+}
