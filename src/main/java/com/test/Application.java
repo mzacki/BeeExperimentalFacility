d17dd8d4f47bfe358a0333d2fd5d61890ae61df2
mathud1 <mateusz.hudzikowski@gmail.com>
2019-04-29 20:04:26 +0200

    thread removed, scheduled added

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -2,12 +2,14 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by Matt on 23.04.2019 at 18:32.
  */
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/test/provider/ScheduledStation.java
@@ -0,0 +1,30 @@
+package com.test.provider;
+
+import com.test.dao.WeatherDataRepository;
+import com.test.entity.WeatherData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Matt on 29.04.2019 at 19:49.
+ */
+
+@Slf4j
+@Component
+public class ScheduledStation {
+
+    @Autowired
+    OwmWeatherProvider owmWeatherProvider;
+    @Autowired
+    WeatherDataRepository weatherDataRepository;
+
+        @Scheduled(fixedRate = 10000)
+        public void checkAndSave() {
+            WeatherData weatherData = owmWeatherProvider.getWeatherData();
+            log.info("Updating...");
+            log.info(weatherData.toString());
+            weatherDataRepository.save(weatherData);
+        }
+}
