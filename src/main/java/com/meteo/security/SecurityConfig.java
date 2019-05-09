ff01e913f08aa2ad070430fe73b0095d85929429
mathud1 <mateusz.hudzikowski@gmail.com>
2019-05-09 10:19:16 +0200

    sba credentials more

--- src/main/java/com/meteo/security/SecurityConfig.java
+++ src/main/java/com/meteo/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.meteo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -10,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 
 @Configuration
-public class SpringConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -22,6 +23,12 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
                 .withUser("admin")
                 .password("{noop}pass")
                 .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .ignoringAntMatchers("/actuator/**");
     }
 
 }
